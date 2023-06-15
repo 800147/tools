cbc04b4f888a1cf386ca78e32824a7f79b742759
800147 <800147@gmail.com>
2023-06-16 01:28:30 +0300

    md-reader update

--- src/Document/md-reader.js
+++ src/Document/md-reader.js
@@ -1,25 +1,16 @@
-// https://highlightjs.org/usage/
-import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/es/highlight.min.js';
-
 // https://markdown-it.github.io/markdown-it/
 import 'https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/dist/markdown-it.min.js';
 
-const main = document.createElement('main');
-
-const onload = () => {
-  document.body.classList.add('Document');
+const onload = async () => {
+  const main = document.createElement('main');
+  const md = document.querySelector('noscript').innerText;
+  main.innerHTML = window.markdownit({ html: true }).render(md);
   document.body.appendChild(main);
+  document.body.classList.add('Document');
+
+  // https://highlightjs.org/usage/
+  const { default: hljs } = await import('https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/es/highlight.min.js');
+  hljs.highlightAll();
 }
 
 document.readyState == 'loading' ? document.addEventListener('DOMContentLoaded', onload) : onload();
-
-(async () => {
-  const pathArray = window.location.pathname.split('/');
-  const fileName = pathArray[pathArray.length - 1]?.replace(/\.\w+$/, '.md') || 'index.md';
-  const file = await fetch(`./${fileName}`).then(res => res.text());
-
-  main.innerHTML = window.markdownit({ html: true }).render(file);
-  document.title = document.querySelector('h1')?.innerText ?? document.title;
-
-  hljs.highlightAll();
-})();
