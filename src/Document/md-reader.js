e99095267413be9d43b1fe3a80ae1c488310fd63
800147 <800147@gmail.com>
2023-10-25 00:08:32 +0300

    noscript>pre selector for markdown in mdreader

--- src/Document/md-reader.js
+++ src/Document/md-reader.js
@@ -1,16 +1,20 @@
 // https://markdown-it.github.io/markdown-it/
-import 'https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/dist/markdown-it.min.js';
+import "https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/dist/markdown-it.min.js";
 
 const onload = async () => {
-  const main = document.createElement('main');
-  const md = document.querySelector('noscript').innerText;
+  const main = document.createElement("main");
+  const md = document.querySelector("noscript>pre").innerText;
   main.innerHTML = window.markdownit({ html: true }).render(md);
   document.body.appendChild(main);
-  document.body.classList.add('Document');
+  document.body.classList.add("Document");
 
   // https://highlightjs.org/usage/
-  const { default: hljs } = await import('https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/es/highlight.min.js');
+  const { default: hljs } = await import(
+    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/es/highlight.min.js"
+  );
   hljs.highlightAll();
-}
+};
 
-document.readyState == 'loading' ? document.addEventListener('DOMContentLoaded', onload) : onload();
+document.readyState == "loading"
+  ? document.addEventListener("DOMContentLoaded", onload)
+  : onload();
